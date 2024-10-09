ca7e58a298598f8cba3f9cba70ba3a1c0504cf49
DevanJoaquin <devan.joaquin@gmail.com>
2024-10-09 13:07:18 -0700

    added delete function

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import TodoList from './TodoList'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
+  const [todos, setTodos] = useState([
+    {id: 1, content: 'Meet Masao for Udon'},
+    {id: 2, content: 'Fix up these horrible React notes'},
+  ]);
+  function deleteTodo(id) {
+    setTodos(todos => todos.filter(todo => todo.id !== id));
+  }
   return (
-    <>
-      <h1>Todo List</h1>
-    </>
-  )
+    <div className="App">
+      <h1>Things To Do</h1>
+      <TodoList todos={todos} deleter={deleteTodo} />
+    </div>
+  );
 }
-
-export default App
